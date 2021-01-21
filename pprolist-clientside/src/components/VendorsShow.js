550e8bc3576166fda434d7f91a0238f8d7f077d2
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-01-20 23:40:31 -0800

    added count functionality using hooks

--- pprolist-clientside/src/components/VendorsShow.js
+++ pprolist-clientside/src/components/VendorsShow.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 // import Vendor from '../components/Vendor'
 import { Link } from 'react-router-dom'
 // import { connect } from 'react-redux'
@@ -7,27 +7,54 @@ import { Link } from 'react-router-dom'
 import vendors from './vendors.css'
 
 const VendorsShow = (props) => {
+  
   const handleDelete = (vendor) => {
     props.deleteVendor(vendor.id);
-  }
+  };
+
+  const [counts, setCounts] = useState({}); // <-- initial empty object for counts
   
+  // similar to componentDidMount:
+  useEffect(() => {
+    document.title = `You clicked ${Object.values(counts).reduce(
+      (total, { count }) => total + count,
+      0
+    )} times`;
+  });
 
+  // initialize/update state when vendors array changes
+  useEffect(() => {
+    setCounts(
+      props.vendors.reduce(
+        (counts, { id }) => ({
+          ...counts,
+          [id]: 0 // <-- store counts by vendor id
+        }),
+        {}
+      )
+    );
+  }, [props.vendors]);
 
+  const handleCount = (id) => () =>
+    setCounts((counts) => ({
+      ...counts,
+      [id]: counts[id] + 1 // <-- update specific vendor's count
+    }));
 
+  
   return (
     <div className="vendor-show-div" style={vendors}>
-      {props.vendors.map((vendor) =>
+      {props.vendors.map((vendor) => (
         <ul key={vendor.id}>
-          <Link to={`/vendors/${vendor.id}`}>{vendor.name}
-              - ${vendor.total_cost}
-              
+          <Link to={`/vendors/${vendor.id}`}>
+            {vendor.name}- ${vendor.total_cost}
           </Link>
-            - <button onClick={() => handleDelete(vendor)}>X</button>
-           
-        </ul>)}
+          - <button onClick={() => handleDelete(vendor)}>X</button>-{" "}
+          <button onClick={handleCount(vendor.id)}>{counts[vendor.id]}</button>
+        </ul>
+      ))}
     </div>
   );
+};
 
-}
-
-export default VendorsShow;
+export default VendorsShow
